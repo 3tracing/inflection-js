b9358972a24734625706a100ebcf41f358d3e86b
Ryan Gasparini <ryan@rxgx.com>
2013-01-27 05:08:42 -0700

    Removed the demo directory.

--- demo/javascripts/demo.js
+++
@@ -1,5 +0,0 @@
-require(['inflection'], function(InflectionJS) {
-    var name = "zephod beeblebrox";
-    console.log("InflectionJS", InflectionJS.capitalize(name));
-    console.log("InflectionJS", InflectionJS.classify(name));
-});
